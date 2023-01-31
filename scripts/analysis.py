905598dacfa13b910064824b87197dd9764d0cff
mukaiyama729 <mukamukatennis7777@icloud.com>
2023-01-31 16:42:09 +0900

    fix

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -7,11 +7,12 @@ import tqdm
 import numpy as np
 import pandas as pd
 from .tessellation import Tessellation
+from .load_file import FileLoader
 
 class Analyzer(Tessellation):
 
     def __init__(self, directory_path):
-        self.data = pd.DataFrame(self.construct_data(directory_path), columns=['time', 'x', 'y', 'z', 'trial', 'cycle', 'reprica'])
+        self.data = pd.DataFrame(FileLoader.construct_data(directory_path), columns=['time', 'x', 'y', 'z', 'trial', 'cycle', 'reprica'])
         self.points = np.array(self.data.loc[:, ['x', 'y', 'z']], dtype='float')
         super().__init__(self.points)
         self.d_result = None
@@ -35,42 +36,3 @@ class Analyzer(Tessellation):
         merged_table = pd.merge(self.data, calculated_table, how='outer', left_index=True, right_index=True)
         self.v_result = merged_table
         return merged_table
-
-    def generate_data(self, data_path):
-        data = []
-        with open(data_path, 'r') as f:
-            read_line = f.readlines()
-
-        name_info_list = self.extract_file_name_info(
-            os.path.basename(data_path)
-        )
-
-        for line in read_line[25:]:
-            record_list = list(map(float, line.replace('\n', '').replace(' ', '').split('\t')))
-            record_list.extend(name_info_list)
-            data.append(record_list)
-
-        return data
-
-    def list_of_files(self, directory_path, suffix):
-        files = glob.glob(os.path.join(directory_path, '*'+ suffix))
-        return files
-
-    def construct_data(self, directory_path, suffix='.txt'):
-        data = []
-
-        for path in self.list_of_files(directory_path, suffix):
-            data.extend(self.generate_data(path))
-
-        return np.array(data)
-
-    def to_float(self, record_list):
-        float_list = []
-        for num in record_list:
-            float_list.append(float(num))
-        return float_list
-
-    def extract_file_name_info(self, file_name):
-        return re.findall(r'\d+', file_name)
-
-
---
+++ scripts/load_file.py
@@ -0,0 +1,50 @@
+import glob
+import os
+import pathlib
+import re
+import os
+import tqdm
+import numpy as np
+import pandas as pd
+class FileLoader:
+
+    @classmethod
+    def generate_data(cls, data_path):
+        data = []
+        with open(data_path, 'r') as f:
+            read_line = f.readlines()
+
+        name_info_list = list(
+            map(float, cls.extract_file_name_info(os.path.basename(data_path)))
+            )
+
+        for line in read_line[25:]:
+            record_list = list(map(float, line.replace('\n', '').replace(' ', '').split('\t')))
+            record_list.extend(name_info_list)
+            data.append(record_list)
+
+        return data
+
+    @staticmethod
+    def list_of_files(directory_path, suffix):
+        files = glob.glob(os.path.join(directory_path, '*'+ suffix))
+        return files
+
+    @classmethod
+    def construct_data(cls, directory_path, suffix='.txt'):
+        data = []
+
+        for path in FileLoader.list_of_files(directory_path, suffix):
+            data.extend(cls.generate_data(path))
+
+        return np.array(data)
+
+    def to_float(self, record_list):
+        float_list = []
+        for num in record_list:
+            float_list.append(float(num))
+        return float_list
+
+    @classmethod
+    def extract_file_name_info(cls, file_name):
+        return re.findall(r'\d+', file_name)
