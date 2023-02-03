2aecc8a0af1cd025ef9f3e8db5292d3424424b02
mukaiyama729 <mukamukatennis7777@icloud.com>
2023-02-03 17:01:45 +0900

    fix

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -6,9 +6,11 @@ import os
 import tqdm
 import numpy as np
 import pandas as pd
+import pickle
 from .tessellation import Tessellation
 from .load_file import FileLoader
 from .arrange_data import ArrangeData
+
 
 class Analyzer(Tessellation):
 
@@ -20,33 +22,74 @@ class Analyzer(Tessellation):
         self.v_result = None
         self.kde_result = None
 
-    def show_data_delaunay(self):
+    def create_delaunay_data(self):
+        self.delaunay_cal()
         calculated_table = self.d_show_data()
         merged_table = pd.merge(self.data, calculated_table, how='outer', left_index=True, right_index=True)
         self.d_result = merged_table
-        return merged_table
 
-    def show_data_kde(self):
+    def create_kde_data(self):
+        self.kernel_density_estimation()
         calculated_table = self.kde_show_data()
         merged_table = pd.merge(self.data, calculated_table, how='outer', left_index=True, right_index=True)
         self.kde_result = merged_table
-        return merged_table
 
-    def show_data_voronoi(self):
+    def create_voronoi_data(self):
+        self.voronoi_cal()
         calculated_table = self.v_show_data()
         merged_table = pd.merge(self.data, calculated_table, how='outer', left_index=True, right_index=True)
         self.v_result = merged_table
-        return merged_table
 
-    def high_volume_d_point(self, percent=10):
+    def large_volume_d_point(self, percent=10):
         self.arranger = ArrangeData(self.d_result)
-        return self.arranger.sorted_points_of_high_volume(percent)
+        return self.arranger.sorted_points_of_large_volume(percent)
 
-    def high_volume_d_trajectories(self):
-        self.arranger = ArrangeData(self.d_result)
-        return self.arranger.averaged_trajectory_data()
+    def large_volume_v_point(self, percent=10):
+        self.arranger = ArrangeData(self.v_result)
+        return self.arranger.sorted_points_of_large_volume(percent)
+
+    def low_density_kde_point(self, percent=10):
+        self.arranger = ArrangeData(self.kde_result)
+        return self.arranger.sorted_points_of_low_density(percent)
+
+    def meaned_trajectory(self, model_name):
+        try:
+            if model_name == 'delaunay':
+                self.arranger = ArrangeData(self.d_result)
+                return self.arranger.averaged_trajectory_data().sort_values('volume', ascending=False)
+            elif model_name == 'voronoi':
+                self.arranger = ArrangeData(self.v_result)
+                return self.arranger.averaged_trajectory_data().sort_values('volume', ascending=False)
+            elif model_name == 'kde':
+                self.arranger = ArrangeData(self.v_result)
+                return self.arranger.averaged_trajectory_data().sort_values('density')
+            else:
+                print('delaunay, voronoi or kde are model_name.')
+                pass
+        except:
+            print('Please make a data using model.')
 
     def is_model(self):
         pass
+
+    def save_instance(self, directory_path, file_name):
+        with open(directory_path + file_name, 'wb') as f:
+            pickle.dump(self, f)
+
+    def save_data(self, df, directory_path, file_name):
+        df.to_csv(directory_path + file_name)
+
+    def calculate_all_model(self):
+        self.create_delaunay_data()
+        self.create_voronoi_data()
+        self.create_kde_data()
+
+    def save_all_data(self, directory_path, instance_file_name, d_file_name, v_file_name, kde_file_name, data_file_name):
+        self.calculate_all_model()
+        self.save_instance(directory_path, instance_file_name)
+        self.save_data(self.d_result, directory_path, d_file_name)
+        self.save_data(self.v_result, directory_path, v_file_name)
+        self.save_data(self.kde_result, directory_path, kde_file_name)
+        self.save_data(self.data, directory_path, data_file_name)
 
 
--- scripts/arrange_data.py
+++ scripts/arrange_data.py
@@ -6,15 +6,21 @@ class ArrangeData:
         self.averaged_data = None
         self.length = len(data)
 
-    def sorted_points_of_high_volume(self, percent):
+    def sorted_points_of_large_volume(self, percent):
         return self.extract_from_top(self.sort_volume(self.data), percent)
         #return self.data.sort_values('volume', ascending=False).head(int(self.length * percent))
 
     def averaged_trajectory_data(self):
         return self.data.groupby(['trial', 'cycle', 'reprica'], as_index=False).mean()
 
-    def sort_volume(self, data):
-        return data.sort_values('volume', ascending=False)
+    def sort_volume(self, data, asc=False):
+        return data.sort_values('volume', ascending=asc)
 
     def extract_from_top(self, data, percent):
         return data.head(int(len(data) * percent * 0.01))
+
+    def sorted_points_of_low_density(self, percent):
+        return self.extract_from_top(self.sort_volume(self.data, asc=True), percent)
+
+    def extract_from_bottom(self, data, percent):
+        return data.tail(int(len(data) * percent * 0.01))
